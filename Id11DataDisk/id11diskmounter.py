149e6b0cc31b82dc9d948fab8c88831349e909ef
Jon Wright <wright@esrf.fr>
2020-10-29 18:08:54 +0100

    added timing and ncalls logging

--- Id11DataDisk/id11diskmounter.py
+++ Id11DataDisk/id11diskmounter.py
@@ -4,15 +4,33 @@
 # https://github.com/clach04/refuse/blob/example/src/examples/loopback.py
 #
 # These imports are from stdlib
-import logging, os, ctypes
+import logging, os, ctypes, collections
 import stat, io, threading, errno
-
+from timeit import default_timer
 from .vendored_refuse.high import FUSE, FuseOSError, Operations, LoggingMixIn
 
 def no_op(self, *args, **kwargs):
     pass  # NOOP
 
-class ID11DiskMounter( LoggingMixIn, Operations):
+class iterstat(object):
+    ik = ['st_atime', 'st_ctime',  'st_gid', 'st_mtime',
+          'st_nlink', 'st_uid', 'st_mode', 'st_size']
+    def __init__(self, vals):
+        self.vals = vals
+    def items(self):
+        for i,k in enumerate(self.ik):
+            yield k, self.vals[i]
+
+def stat_to_iter(st):
+    return iterstat( [st.st_atime, st.st_ctime,  st.st_gid, st.st_mtime,
+                      st.st_nlink, st.st_uid, st.st_mode, st.st_size] )
+
+
+class ID11DiskMounter( Operations):
+
+    CALLS = collections.defaultdict( int )
+    ELAPS = collections.defaultdict( float )
+    GAS = collections.defaultdict( int )
 
     def __init__(self, root, data):
         """
@@ -24,12 +42,12 @@ class ID11DiskMounter( LoggingMixIn, Operations):
         self.data = data
         #
         st = os.lstat(root) # folder stat defaults for creation time etc
-        self.st = dict((key, getattr(st, key)) for key in (
+        vals = [ getattr(st, key) for key in (
             'st_atime', 'st_ctime', 'st_gid', 'st_mtime',
-            'st_nlink', 'st_uid'))
-        self.st['st_mode'] = stat.S_IFREG | 0o444 # everyone can read files
-        self.st['st_size'] = self.data.filesize() # assume all are the same size
-        #
+            'st_nlink', 'st_uid')]
+        vals.append(stat.S_IFREG | 0o444) # everyone can read files
+        vals.append(self.data.filesize()) # assume all are the same size
+        self.st = iterstat( vals )
         # Create a mapping of file descriptors, system files versus our
         # fake files
         # keys in here are the open files
@@ -39,8 +57,22 @@ class ID11DiskMounter( LoggingMixIn, Operations):
 
     def __call__(self, op, path, *args):
         """ for FUSE """
-        return super(ID11DiskMounter, self).__call__(
+        self.CALLS[op] += 1
+        start = default_timer()
+        ret = super(ID11DiskMounter, self).__call__(
             op, self.root + path, *args)
+        end = default_timer()
+        self.ELAPS[op] += (end-start)
+        return ret
+
+    def __del__(self):
+        print("Runtime statistics")
+        print("Operation   Ncalls   Time")
+        for key in self.CALLS.keys():
+            print(key,self.CALLS[key],self.ELAPS[key],self.ELAPS[key]/self.CALLS[key])
+        print("Getattr calls")
+        for key in self.GAS.keys():
+            print(key,self.GAS[key])
 
     def access(self, path, mode):
         if not os.access(path, mode):
@@ -49,27 +81,27 @@ class ID11DiskMounter( LoggingMixIn, Operations):
     def open(self, path, *args, **kwds):
         """ Open a file - system pass through or one of ours """
         with self.rwlock: # because we modify openfds
-            direc, fname = os.path.split( path )
             # new file descriptor is:
             outfd = max(self.openfds.keys()) + 1
-            if fname in self.data.filenames and not os.path.exists(path):
-                self.openfds[outfd] = fname
-            else:
-                flags = args[0]
-                if hasattr(os, "O_BINARY") and not (args[0] & os.O_TEXT):
-                    flags |= os.O_BINARY
-                newfd = os.open(path, flags, **kwds)
-                self.openfds[outfd] = newfd
+            if path.endswith(self.data.extn):
+                direc, fname = os.path.split( path )
+                if fname in self.data.filenames and not os.path.exists(path):
+                    self.openfds[outfd] = fname
+                    return outfd
+            flags = args[0]
+            if hasattr(os, "O_BINARY") and not (args[0] & os.O_TEXT):
+                flags |= os.O_BINARY
+            newfd = os.open(path, flags, **kwds)
+            self.openfds[outfd] = newfd
             return outfd
 
     def read(self, path, size, offset, outfd):
         """ Read a file, system pass through or one of ours """
         with self.rwlock:
-            if outfd in self.openfds: # file is open
-                sysfd = self.openfds[outfd]
-                if isinstance( sysfd, int ):
-                    os.lseek(sysfd, offset, 0)
-                    return os.read(sysfd, size)
+            sysfd = self.openfds[outfd]
+            if isinstance( sysfd, int ):
+                os.lseek(sysfd, offset, 0)
+                return os.read(sysfd, size)
         # free the lock now, this is slow but not vulnerable to writes
         frm = self.data[sysfd]
         if offset > len(frm):
@@ -122,33 +154,41 @@ class ID11DiskMounter( LoggingMixIn, Operations):
         with self.rwlock:
             if fh in self.openfds:
                 sysfd = self.openfds[fh]
-                if sysfd in self.data.filenames:
-                    return 0
-                else:
+                if isinstance(sysfd, int):
                     if datasync != 0:
                         return os.fdatasync(sysfd)
                     else:
                         return os.fsync(sysfd)
+                else:
+                    return 0
 
     def readdir(self, path, fh):
         with self.rwlock:
             p = os.path.realpath(path)
             locals = ['.', '..']
             if p == self.root:
-                locals += self.data.filenames
+                locals += self.data.filename_list
             return locals +  os.listdir(path)
 
     def getattr(self, path, fh=None):
         # FIXME the st_gid and st_uid are problematic still
-        direc, fname = os.path.split( path )
-        if fname in self.data.filenames and not os.path.exists(path):
-            std = self.st
+        if fh is not None:
+            sysfd = self.openfds[fh]
+            if isinstance( sysfd, int ):
+                st = os.fstat( sysfd )
+                self.GAS['fstat']+=1
+                return stat_to_iter( st )
+            self.GAS['fake']+=1
+            return self.st
         else:
+            if path.endswith( self.data.extn ):
+                direc, fname = os.path.split( path )
+                if fname in self.data.filenames and not os.path.exists(path):
+                    self.GAS['fake']+=1
+                    return self.st
             st = os.lstat(path)
-            std = dict((key, getattr(st, key)) for key in (
-            'st_atime', 'st_ctime', 'st_gid', 'st_mode', 'st_mtime',
-            'st_nlink', 'st_size', 'st_uid'))
-        return std
+            self.GAS['lstat']+=1
+            return stat_to_iter( st )
 
     def release(self, path, fh):
         """ Closes a file """
@@ -168,10 +208,11 @@ class ID11DiskMounter( LoggingMixIn, Operations):
         with self.rwlock:
             if fh in self.openfds:
                 sysfd = self.openfds[fh]
-                if sysfd in self.data.filenames:
+                if isinstance( sysfd, int ):
+                    pos = os.lseek(sysfd, offset, 0)
+                    return os.write(sysfd, data)
+                else:
                     logging.ERROR("You cannot write these! %s %d"%(path, fh))
                     raise FuseOSError(errno.EPERM)
-                pos = os.lseek(sysfd, offset, 0)
-                return os.write(sysfd, data)
 
 
