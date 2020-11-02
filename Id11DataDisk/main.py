65b52576e68bf28c148558c039d97642b176c4f2
Jon Wright <jonathan.wright@gmail.com>
2020-11-02 13:46:24 +0100

    add beam cen and distance

--- Id11DataDisk/main.py
+++ Id11DataDisk/main.py
@@ -1,12 +1,15 @@
 
 
-from . import id11diskmounter, files_from_3d, id11esperanto
+from . import id11diskmounter, files_from_3d, id11esperanto, LRU_CACHE_SIZE
 import argparse, logging, sys, os
 
 def main(args):
 
     for k,v in vars(args).items():
         print(k,':',v)
+
+    if args.dump:
+        LRU_CACHE_SIZE=1
 
     if args.format == 'edf':
         data = files_from_3d.EdfFrom3d( args.h5file, args.scan, **vars(args) )
@@ -17,6 +20,18 @@ def main(args):
     else:
         print("Not recognised format ",args.format)
         sys.exit()
+
+    if args.dump:
+        if not os.path.exists( args.mount ):
+            os.mkdir( args.mount )
+        print("Dumping files into", args.mount)
+        for filename in data.filename_list:
+            outname = os.path.join( args.mount, filename)
+            print( outname )
+            with open( outname, "wb" ) as fout:
+                fout.write( data[filename] )
+        sys.exit(0)
+
 
     root = args.mount + "_real"
     # root is a real existing disk folder
@@ -67,7 +82,11 @@ def makeArgs():
     parser.add_argument("--stepangle", dest='stepangle', default = 0.25)
     parser.add_argument("--expotime", dest='expotime', default=1.0)
     parser.add_argument("--wavelength", dest='wavelength', default=0.308)
+    parser.add_argument("--x0", dest='x0', default=1024.)
+    parser.add_argument("--y0", dest='y0', default=1024.)
+    parser.add_argument("--distance", dest='distance', default=140.)
     parser.add_argument("--run", dest='run', default = 1)
+    parser.add_argument("--dump", dest='dump', default=False, action='store_true')
     return parser
 
 if __name__ == '__main__':
